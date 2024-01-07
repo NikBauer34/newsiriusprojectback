bc580daddc8938e22b4930b770d3f941b4a8a0b8
NikBauer34 <nik.bauer.09@mail.ru>
2024-01-07 17:18:47 +0300

    new2

--- src/modules/constants/index.ts
+++ src/modules/constants/index.ts
@@ -1,1 +1,7 @@
+import { publicRouting, firstRouting, secondRouting } from "./Routing"
+export {
+  publicRouting,
+  firstRouting,
+  secondRouting
+}
 export const API_URL = 'http://localhost:4000'
