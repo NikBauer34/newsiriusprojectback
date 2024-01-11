8a4af8c23be7b0fcd04424362e2d576793e016da
TamaraKK <tomkavins@gmail.com>
2024-01-11 17:05:47 +0500

    add log files

--- src/modules/components/Auth/LoginForm.ts
+++ src/modules/components/Auth/LoginForm.ts
@@ -1,2 +1,10 @@
+import { useForm } from "@mantine/form";
 
-
+const LoginForm = useForm({
+    initialValues: {nikname: '', password: ''},
+    validateInputOnChange: true,
+    validate: {
+        nikname: (value) => (value.length<5 ? 'Никнейм должен состоять хотя-бы из 5 символов' : null),
+        
+    }
+})
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,14 @@
+import { Box } from "@mantine/core";
+import React, { FC } from "react";
 
+const Login: FC = () => {
+    return(
+        <Box maw={340} mx= 'auto'>
+            <form>
 
+            </form>
+        </Box>
+    );
+};
 
-
+export default Login;
